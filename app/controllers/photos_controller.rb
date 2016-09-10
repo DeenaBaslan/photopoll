5d729f158f1c525cab707d616934ea5c43ca7808
DeenaBaslan <nuraddinabaslan.nab@gmail.com>
2016-09-10 12:26:10 +0800

    Update photos_controller.rb

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,20 @@
 class PhotosController < ApplicationController
+  def new
+      @photo = Photo.new
+    end
+
+    def create
+      @photo = Photo.new(photo_params)
+      if @photo.save
+      else
+        render :new
+      end
+    end
+
+    private
+      def photo_params
+        params.require(:photo).permit(:file, :caption)
+      end
   def index
     @photos = Photo.all
     begin
